6312caa4bb6190eaae05e73618d74b48333d3f53
Tadgh Purcell <tadghp@gmail.com>
2023-04-19 13:44:56 +0100

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -5,37 +5,20 @@ const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
 
-// let persons = [
-//   {
-//     id: 1,
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: 2,
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: 3,
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: 4,
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-// ];
-
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 app.use(cors());
 
 app.use(express.static("build"));
 
 app.use(express.json());
-morgan.token("type", (req, res, method) => {
-  return JSON.stringify(req);
-});
+
+app.use(requestLogger);
 
 app.use(
   morgan(function (tokens, req, res) {
@@ -55,62 +38,88 @@ morgan.token("addContact", () => {
   return "";
 });
 
-// const findPerson = (id) => persons.find((person) => person.id === +id);
-
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((persons) => res.json(persons));
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  Person.findById(req.params.id).then((person) => res.json(person));
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  Person.findById(req.params.id).then((person) => res.json(person));
-  res.status(204).end();
-});
-
-app.post("/api/persons", (req, res) => {
+app.put("/api/persons/:id", (req, res, next) => {
   const body = req.body;
 
-  // const sameName = persons.some(
-  //   (person) => person.name.toLowerCase() === body.name?.toLowerCase()
-  // );
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
 
-  const sameName = Person.find({}).then((persons) =>
-    persons.some((person) => {
-      console.log(person);
-      console.log(body);
-      return person.name.toLowerCase() === body.name?.toLowerCase();
-    })
-  );
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => res.json(updatedPerson))
+    .catch((error) => next(error));
+});
 
-  console.log(sameName);
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => res.status(204).end())
+    .catch((error) => next(error));
+});
 
-  if (!body.name) {
-    return res.status(404).json({ error: "name missing" });
-  }
-  if (!body.number) {
-    return res.status(404).json({ error: "number missing" });
-  }
+app.post("/api/persons", (req, res, next) => {
+  const body = req.body;
 
-  // if (sameName) {
-  //   return res.status(400).json({ error: "name must be unique" });
+  // Person.find({}).then((persons) => {
+  //   if (
+  //     persons.some(
+  //       (person) => person.name.toLowerCase() === body.name?.toLowerCase()
+  //     )
+  //   )
+  //     return res.status(400).json({ error: "name must be unique" });
+  // });
+
+  // if (!body.name) {
+  //   return res.status(404).json({ error: "name missing" });
+  // }
+  // if (!body.number) {
+  //   return res.status(404).json({ error: "number missing" });
   // }
 
   const person = new Person({
-    name: process.argv[3],
-    number: process.argv[4],
+    name: body.name,
+    number: body.number,
   });
 
   const { name, number } = person;
 
+  person.save().then((savedPerson) => {
+    return res.send(savedPerson);
+  });
+
   morgan.token("addContact", () => {
     return JSON.stringify({ name, number });
   });
-
-  person.save().then((savedPerson) => response.json(savedPerson));
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  console.error(error.name);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
--- backend/models/person.js
+++ backend/models/person.js
@@ -17,10 +17,10 @@ const personSchema = new mongoose.Schema({
 });
 
 personSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
-    delete returnedObject.__v;
   },
 });
 
